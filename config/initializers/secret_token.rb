a3db91bfcb80ad2069c0c48fa53e722c844bfcc6
Conrad Lesiak <kaisel_inc@yahoo.com>
2014-12-26 19:57:16 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Radgamestudios::Application.config.secret_key_base = '7388c64f825119328871f39a694cb79978b9a65d26e8615e45d8a24a65e0bc34bb6a6678f2a4d96cf5903872e81c95d7017e8948e098ae52331ccc273a88d253'
